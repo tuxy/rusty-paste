368e0cf61033d629adefdf5d6f077f69a49f9891
tuxy <lastpass7565@gmail.com>
2024-12-06 17:26:10 +0700

    Basic server post & storage

--- src/server/bin/main.rs
+++ src/server/bin/main.rs
@@ -1,16 +1,44 @@
 use tiny_http::{Server, Response};
+use serde::Deserialize;
+use std::{fs, cmp};
+use nanoid::nanoid;
+
+#[derive(Deserialize)]
+struct Config {
+    bind_address: String,
+    paste_limit: usize,
+}
+
+impl Config {
+    fn open_config() -> Config {
+        let config = fs::read_to_string("config.toml").unwrap();
+        return toml::from_str(&config).unwrap();
+    }
+}
 
 fn main() {
-    let server = Server::http("127.0.0.1:8765").unwrap();
+    let config = Config::open_config();
+    let mut paste_data: Vec<[String; 2]> = Vec::with_capacity(config.paste_limit);
+    let server = Server::http(config.bind_address).unwrap();
 
-    for request in server.incoming_requests() {
-        println!("received request! method: {:?}, url: {:?}, headers: {:?}",
-            request.method(),
-            request.url(),
-            request.headers()
-        );
-    
-        let response = Response::from_string("hello world");
-        let _ = request.respond(response);
+    for mut request in server.incoming_requests() {
+
+        let mut content = String::new();
+        request
+            .as_reader()
+            .read_to_string(&mut content)
+            .unwrap();
+        
+        match  paste_data.len().cmp(&config.paste_limit) {
+            cmp::Ordering::Less => {
+                let id = nanoid!(8);
+                paste_data.push([String::from(&id), String::from(content)]);
+
+                println!("{:#?}", paste_data);
+                let response = Response::from_string(id);
+                let _ = request.respond(response);
+            },
+            _ => todo!()
+        }
     }
 }
