ebc2b076740d4305f64aee562f85a2a89d8d5fc2
tuxy <lastpass7565@gmail.com>
2024-12-07 08:43:24 +0700

    Initial client code

--- src/client/bin/main.rs
+++ src/client/bin/main.rs
@@ -1,3 +1,16 @@
+use ureq;
+use clap::{Arg, Parser};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    url: String,
+    content: String,
+}
+
 fn main() {
+    let args = Args::parse();
+
     println!("Hello World!");
 }
